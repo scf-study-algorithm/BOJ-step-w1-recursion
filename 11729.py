01f5cc981e8c1d1e4f6d2ce0619d8c3b0ef9be63
Miso Kim <kimms9725@naver.com>
2021-12-24 10:35:17 +0900

    21/12/24 solved.

--- 11729.py
+++ 11729.py
@@ -1,9 +1,5 @@
 n = int(input())
 
-a = [i for i in range(n, 0, -1)];
-b = [];
-c = [];
-
 def hanoi(n, start, to, via):
     if n == 1:
         print(start, to)
@@ -12,5 +8,5 @@ def hanoi(n, start, to, via):
         hanoi(n-1, start, via, to)
         print(start, to)
         hanoi(n-1, via, to, start)
-        
+print(2**n - 1)
 hanoi(n, "1", "3", "2")
